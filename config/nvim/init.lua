810900b2b08979d7797d11e4b009cd83e69771b6
mhbidhan <mhbidhan5@gmail.com>
2026-01-03 16:36:08 +0600

    Deleted lua based neovim config

--- config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
--- Global settings
-require("configs.global").config()
-
--- Lazy bootstrap
-require("configs.lazy")
-
--- Additional configs
-require("configs.telescope").config()
-require("configs.lsp").config()
--- config/nvim/lua/plugins/git-signs.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup({})
-    vim.cmd("Gitsigns toggle_current_line_blame")
-    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-  end,
-}
