7cfb74eea0a23ee35f970aecfbd8ebfb43a3f698
mhbidhan <mhbidhan5@gmail.com>
2026-01-22 15:21:34 +0600

    Full system upgrade
    - Locked hydenix to a specific commit to stay on hyprland 0.52.2 to
    avoid sytanx errors on config files
    - Upgraded all packages except for hydenix

--- flake.nix
+++ flake.nix
@@ -8,9 +8,7 @@
 
     # Extras
     neovim-flake.url = "github:mhbidhan/neovim-flake";
-    hydenix = {
-      url = "github:richen604/hydenix";
-    };
+    hydenix.url = "github:richen604/hydenix/f47e7d57e6c8f0e7ce60735e975392c762376a4a";
   };
 
   outputs = {nixpkgs, ...} @ inputs: let
